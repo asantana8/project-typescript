7834cee52b1a199bdb40e9b16a52ca7e44c0b93c
Adriano Cunha de Santana <t_acsantana@trf5.jus.br>
2022-07-10 16:56:41 -0300

    Resolução desafio 1

---
+++ scr/desafio/desafio1.ts
@@ -0,0 +1,23 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+/*
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+*/
+
+//Criando inteface
+interface Employee { 
+    code : number, 
+    name : string 
+};
+
+const funcionarioObj = {} as Employee;
+funcionarioObj.code = 19;
+funcionarioObj.name = "Adriano Santana";
+
+const funcionarioObj2 : Employee = {
+    code : 22,
+    name : 'Maiely Janys'
+}
+
+console.log(`Funcionário 1 ${funcionarioObj} e funcionário 2 ${funcionarioObj2}.`);
